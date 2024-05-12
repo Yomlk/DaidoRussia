27039ea8a2e012e5f0a81c4643e314d9e6e2039f
Yomlk <lastik52@mail.ru>
2024-05-12 20:20:52 +0300

    Header complete

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,14 +3,47 @@ import './style.css'
 export default function Header() {
     return(
         <>
-    <div className='header'>
-        <div className="logo">
-            <img src="./src/components/Header/img/image 1.png"  />
-        </div>
-        <div className="nav">
+    <header>
+        <div className="container-fluid First-header">
+            <div className="row">
+                <div className="hamburger">
+                    <div className="hamburger-menu">
+                        <input id="menu__toggle" type="checkbox" />
+                        <label className="menu__btn" htmlFor="menu__toggle">
+                         <span></span>
+                        </label>
 
+                        <ul className="menu__box">
+                         <li><a className="menu__item" href="#">Home</a></li>
+                         <li><a className="menu__item" href="#">About</a></li>
+                         <li><a className="menu__item" href="#">Team</a></li>
+                         <li><a className="menu__item" href="#">Contact</a></li>
+                         
+                        </ul>
+                    </div>
+                </div>
+                <div className="col-xxl-2 col-xl-2 col-lg-2 logo">
+                    <div className="log">
+                     <img src="./src/components/Header/img/image 1.png" className='imglogo'/>
+                    </div>
+                </div>
+                <div className="col-xxl-6 col-xl-8 col-lg-8 nav">
+                   <ul className='nav-item'>
+                     <li>О компании</li>
+                     <li>Продукция</li>
+                     <li>Проверка подлинности</li>
+                     <li>Контакты</li>
+                    </ul>
+                </div>
+                <div className="col-xxl-4 col-xl-2 col-lg-2 country">
+                  <img src="./src/components/Header/img/rus.svg" className='icon' />
+                  <img src="./src/components/Header/img/usa.svg" className='icon' />
+                </div>
+            </div>
+           
         </div>
-    </div>
+        
+    </header>
     </>
     )
     
--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/Swiperblk/App'
 import Header from './components/Header/Header'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/null.css'
 import './styles/fonts.css'
+import './components/Header/media.css'
 
 
 
